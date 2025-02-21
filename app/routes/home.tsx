0e5573d87b552a3456d75750e3691702dedd2cbd
Jerry Qiu <jqiu61@stuy.edu>
2025-02-20 20:51:24 -0500

    meta edit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Hackamail" },
+    { name: "description", content: "Welcome to the work-in-progress website for Hackamail!" },
   ];
 }
 
